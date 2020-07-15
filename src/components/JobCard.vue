d9cab41ddd8a6a621c299a8d8be6f7ecd9be1843
gergokutu <gergo.kutu@gmail.com>
2020-07-15 12:54:10 +0200

    Place 1 JobCard into Home

---
+++ src/components/JobCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+     <b-col>
+          <b-card
+            title="This is a Card"
+            img-src="https://picsum.photos/600/300/?image=36"
+            img-alt="Job-info"
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2"
+          >
+            <b-card-text>Text...Text...Text...</b-card-text>
+            <b-button variant="primary">Apply</b-button>
+          </b-card>
+        </b-col>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,83 @@
 <template>
   <div class="home">
+    <b-container>
+      
+      <b-row align-v="center">
+        <job-card></job-card>
+        <!-- <b-col md="3">
+          <b-card
+            title="This is a Card"
+            img-src="https://picsum.photos/600/300/?image=36"
+            img-alt="Job-info"
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2"
+          >
+            <b-card-text>Text...Text...Text...</b-card-text>
+            <b-button variant="primary">Apply</b-button>
+          </b-card>
+        </b-col>
+
+        <b-col md="3">
+          <b-card
+            title="This is a Card"
+            img-src="https://picsum.photos/600/300/?image=36"
+            img-alt="Job-info"
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2"
+          >
+            <b-card-text>Text...Text...Text...</b-card-text>
+            <b-button variant="primary">Apply</b-button>
+          </b-card>
+        </b-col>
+
+        <b-col md="3">
+          <b-card
+            title="This is a Card"
+            img-src="https://picsum.photos/600/300/?image=36"
+            img-alt="Job-info"
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2"
+          >
+            <b-card-text>Text...Text...Text...</b-card-text>
+            <b-button variant="primary">Apply</b-button>
+          </b-card>
+        </b-col>
+
+        <b-col md="3">
+          <b-card
+            title="This is a Card"
+            img-src="https://picsum.photos/600/300/?image=36"
+            img-alt="Job-info"
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2"
+          >
+            <b-card-text>Text...Text...Text...</b-card-text>
+            <b-button variant="primary">Apply</b-button>
+          </b-card>
+        </b-col> -->
+      </b-row>
+
+      
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import JobCard from '@/components/JobCard.vue'
 
 export default {
   name: "Home",
   components: {
+    "job-card": JobCard
   }
 };
 </script>
